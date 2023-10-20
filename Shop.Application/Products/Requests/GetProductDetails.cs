b2db2685ce09d32a9e6d59b64f082cb9b4807671
Андрей Кузнецов <blacksilver@ro.ru>
2023-10-21 02:39:29 +0300

    Добавление запросов для получения информации о товаре и списка товаров

---
+++ Shop.Application/Products/Requests/GetProductDetails.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shop.Application.Common.Exceptions;
+using Shop.Application.Interfaces;
+using Shop.Domain.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Shop.Application.Products.Requests
+{
+    public class GetProductDetailsRequest : IRequest<Product>
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class GetProductDetailsRequestHandler : IRequestHandler<GetProductDetailsRequest, Product>
+    {
+        private readonly IShopDbContext _dbContext;
+
+        public GetProductDetailsRequestHandler(IShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Product> Handle(GetProductDetailsRequest request, CancellationToken cancellationToken)
+        {
+            var product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+
+            if (product == null)
+            {
+                throw new NotFoundException(nameof(product), request.Id);
+            }
+
+            return product;
+        }
+    }
+}
---
+++ Shop.Application/Products/Requests/GetProductsListRequest.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Shop.Application.Interfaces;
+using Shop.Domain.Models;
+
+namespace Shop.Application.Products.Requests
+{
+    public class GetProductsListRequest : IRequest<List<Product>>
+    {
+    }
+
+    public class GetProductsListRequestHandler : IRequestHandler<GetProductsListRequest, List<Product>>
+    {
+        private readonly IShopDbContext _dbContext;
+
+        public GetProductsListRequestHandler(IShopDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Product>> Handle(GetProductsListRequest request, CancellationToken cancellationToken)
+        {
+            return await _dbContext.Products.ToListAsync(cancellationToken);
+        }
+    }
+}
