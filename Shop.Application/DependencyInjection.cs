4e496a1a431dd33fbf2f532adfb37df3c37d5970
Андрей Кузнецов <blacksilver@ro.ru>
2023-10-24 00:32:16 +0300

    Добавление Dependency Injection

---
+++ Shop.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Shop.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ Shop.Database/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Shop.Application.Interfaces;
+
+namespace Shop.Database
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["DbConnection"];
+            
+            services.AddDbContext<ShopDbContext>(options =>
+            {
+                options.UseNpgsql(connectionString);
+            });
+
+            services.AddScoped<IShopDbContext>(provider =>
+                provider.GetService<ShopDbContext>());
+
+            return services;
+        }
+    }
+}
